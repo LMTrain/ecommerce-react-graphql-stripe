4102ee9c24be6d3e79c314650583a8cc0025348e
LM Train <layconmm@msn.com>
2019-11-13 12:18:25 -0600

    Registered User, store JWT in LocalStorage

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,7 @@
 const CART_KEY = 'cart';
+const TOKEN_KEY = 'jwt';
+
+
 export const calculatePrice = items => {
     return `$${items
         .reduce((acc, item) => acc + item.quantity * item.price, 0)
@@ -18,4 +21,11 @@ export const getCart = (cartKey = CART_KEY) => {
         return JSON.parse(localStorage.getItem(cartKey));
     }
     return [];
+};
+
+/* Auth */
+export const setToken = (value, tokenKey = TOKEN_KEY) => {
+    if (localStorage) {
+        localStorage.setItem(tokenKey, JSON.stringify(value));
+    }
 }
